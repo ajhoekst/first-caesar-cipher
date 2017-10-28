eee2efe6f518918fc967886c41f1aa31ac041bb5
Andrew Hoekstra <git@andrewhoekstra.com>
2017-10-28 10:05:09 -0400

    Add submodule structure

--- src/caesar.cpp
+++ src/caesar.cpp
@@ -1,3 +1,9 @@
+#define __ahio
+
+#include "caesar.h"
+
+#include "file-mmap.h"
+
 #include <iostream>
 
 int main(
@@ -5,7 +11,14 @@ int main(
     char** argv
 )
 {
-    std::cout << "Hello world!" << std::endl;
+    ahio::file_map m;
+    ahio::map_file(
+        &m,
+        argv[1],
+        FILE_MAP_R_PRIVATE
+    );
+    printf("File contents:\n%s\n", m.address);
+    ahio::unmap_file( &m );
 
     return 0;
 }
